3732cf30e73369cd419248276c7a5dd8d9353cb1
nda <147893520+marwanada28@users.noreply.github.com>
2025-07-02 18:16:02 +0700

    TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,20 +3,22 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class TicTacToe extends JFrame implements ActionListener {
-    JButton[] buttons = new JButton[9];
-    boolean turnX = true;
+    JButton[][] tombol = new JButton[3][3];
+    boolean giliranX = true;
 
     public TicTacToe() {
-        setTitle("Tic-Tac-Toe");
+        setTitle("Tic Tac Toe");
         setSize(300, 300);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new GridLayout(3, 3));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        for (int i = 0; i < 9; i++) {
-            buttons[i] = new JButton("");
-            buttons[i].setFont(new Font("Arial", Font.BOLD, 40));
-            buttons[i].addActionListener(this);
-            add(buttons[i]);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tombol[i][j] = new JButton("");
+                tombol[i][j].setFont(new Font("Arial", Font.BOLD, 40));
+                tombol[i][j].addActionListener(this);
+                add(tombol[i][j]);
+            }
         }
 
         setVisible(true);
@@ -25,8 +27,12 @@ public class TicTacToe extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         JButton btn = (JButton) e.getSource();
         if (btn.getText().equals("")) {
-            btn.setText(turnX ? "X" : "O");
-            turnX = !turnX;
+            if (giliranX) {
+                btn.setText("X");
+            } else {
+                btn.setText("O");
+            }
+            giliranX = !giliranX;
         }
     }
 
